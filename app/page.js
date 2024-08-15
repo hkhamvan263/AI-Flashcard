127309b49622c8cf314b8df4795502c6572f1402
Hannah Khamvan <92757465+hkhamvan263@users.noreply.github.com>
2024-08-15 00:51:26 +0000

    Commented out getStripe import

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 'use client'
 import Image from "next/image";
-import getStripe from "@/utils/getStripe";
+// import getStripe from "@/utils/get-stripe";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { AppBar, Button, Container, Toolbar, Typography} from "@mui/material";
 import next from "next";
